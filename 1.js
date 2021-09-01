44d2aa70cb934bb032ab5f5725215f851c0bdf20
Anna Miroshnichenko <miroshnichenkoanna74@gmail.com>
2021-09-01 20:50:42 +0500

    123

---
+++ 1.js
@@ -0,0 +1,3 @@
+intent('$(op1 multiply|summarize) $(nu NUMBER) and $(n2 NUMBER)', p => {
+    p.play('(hello|hi there)');
+}); 
---
+++ HelloWordcopy10.js
@@ -0,0 +1,8 @@
+// Use this sample to create your own voice commands
+intent('hhhi', p=>  {
+    p.play('huuui Anna');
+});
+
+intent('I want to live in $(LOC)', p=>  {
+    p.play('Me too1');
+});
